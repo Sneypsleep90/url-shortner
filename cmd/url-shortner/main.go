4c7de6e5735335b89b9442c385f7b7e15006a5f1
magomed <magamed4924@mail.ru>
2025-09-30 13:28:05 +0300

    made geturl

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url-shortner/internal/config"
+	"url-shortner/internal/lib/logger/sl"
+	"url-shortner/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("start url-shortner")
+	log.Debug("some debug stuff")
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
